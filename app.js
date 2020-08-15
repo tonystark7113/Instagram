ef346d4d203f0ebb0c1ba638e30032144459554b
tonystark7113 <69718632+tonystark7113@users.noreply.github.com>
2020-08-15 17:46:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAWg8Cp_k8-A2Z5xBj08_ln1NWj0C6Zrtk",
+  authDomain: "hacking-df61a.firebaseapp.com",
+  databaseURL: "https://hacking-df61a.firebaseio.com",
+  projectId: "hacking-df61a",
+  storageBucket: "hacking-df61a.appspot.com",
+  messagingSenderId: "743036809413",
+  appId: "1:743036809413:web:46982547633238ee8440ac",
+  measurementId: "G-BY9F7MF4JM"
 };
 
      
